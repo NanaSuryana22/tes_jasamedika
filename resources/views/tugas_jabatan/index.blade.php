9aa90233d3c6a254c2fd143b1e47be8c34d32aaa
Nana Suryana <nanasuryana554@gmail.com>
2021-11-09 12:58:29 +0700

    Bugfixed

--- resources/views/tugas_jabatan/index.blade.php
+++ resources/views/tugas_jabatan/index.blade.php
@@ -5,7 +5,7 @@
 <div class="page-title">
   <div class="row">
       <div class="col-12 col-md-6 order-md-1 order-last">
-          <h3>Jabatan</h3>
+          <h3>Tugas Jabatan</h3>
           <p class="text-subtitle text-muted">Koleksi Data Tugas Jabatan.</p>
       </div>
       <div class="col-12 col-md-6 order-md-2 order-first">
